e78c3b686f44adcef07b55fb26662b486865400d
pranaypareek <pranay.metal@gmail.com>
2017-08-01 23:27:53 -0700

    removes old spawn code from spawnChild

--- exec/spawnChild.js
+++ exec/spawnChild.js
@@ -39,50 +39,6 @@ function _prepareCMD(next) {
   return next();
 }
 
-/*
-//spawn the child process and execute the script
-function _spawnChild(next) {
-  console.log('Inside ----', _spawnChild.name);
-
-  const spawnProcess = child_process.spawn(bag.cmd, bag.args);
-  spawnProcess.stdout.setEncoding('utf8');
-
-  bag.res = '';
-
-  console.log('\n------------------');
-  console.log('Executing:', bag.cmd, bag.args);
-  console.log('------------------');
-
-  //collate data coming from the spawn stream
-  spawnProcess.stdout.on('data', function(data) {
-    let str = data.toString();
-    let lines = str.split(/(\r?\n)/g);
-    bag.res += lines.join("");
-  });
-
-  //if error, store the error object in the bag variable
-  spawnProcess.on('error', function(err) {
-    console.log("Error processing the child process:", err);
-    bag.error = err;
-  });
-
-  //prepare final response when the process exits
-  spawnProcess.on('close', function(code) {
-    console.log('Process exited with code:', code);
-    if (!_.isEmpty(bag.error)) {
-      return next(bag.error);
-    } else {
-      console.log('\n------------------');
-      console.log('Processed!');
-      console.log('------------------');
-
-      console.log(bag.res);
-      return next();
-    }
-  });
-}
-*/
-
 //spawn the child process and execute the script
 function _spawnChild(next) {
   console.log('Inside ----', _spawnChild.name);
