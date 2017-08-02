a6971aed2e145856d38a3d0d71a6593576a4d421
pranaypareek <pranay.metal@gmail.com>
2017-08-02 12:48:18 -0700

    updates controller to publish request message

--- gateway/controllers/controllers.js
+++ gateway/controllers/controllers.js
@@ -1,12 +1,26 @@
 'use strict';
 
+var amqp = require('amqplib/callback_api');
+
 exports.echoReq = function(req, res) {
   console.log('Received GET:\n', req);
   res.send('OK');
 };
 
 exports.publishReq = function(req, res) {
-  const resBody = req.body;
-  console.log('Published:\n', resBody);
-  res.send('OK');
+  const reqBody = req.body;
+  console.log('Published:\n', reqBody);
+
+  amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+      var q = 'hello';
+      var msg = reqBody.runtime;
+
+      ch.assertQueue(q, {durable: false});
+      ch.sendToQueue(q, new Buffer(msg));
+      //console.log('Runtime requested: %s', msg);
+      res.send('OK');
+    });
+    setTimeout(function() { conn.close(); process.exit(0); res.send('OK');}, 500);
+  });
 };
